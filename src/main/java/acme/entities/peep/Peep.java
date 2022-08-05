3875afcc30309608cebe8cd634df1a7a62b755db
Josemari <Josemari@PC-JM.home>
2022-08-05 18:35:53 +0200

    Tarea002: Done

--- src/main/java/acme/entities/peep/Peep.java
+++ src/main/java/acme/entities/peep/Peep.java
@@ -33,8 +33,8 @@ public class Peep extends AbstractEntity {
 
     @NotBlank
     @Length(min=1, max=100)
-    protected String writer;
-
+    protected String writer; 
+    
     @NotBlank
     @Length(min=1, max=100)
     protected String heading;
