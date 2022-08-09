72f3b7cefda64bbf01350d20d92fcfb3b3553bd9
Eloy Moreno Dominguez <elmodo10@gmail.com>
2022-08-09 20:00:56 +0200

    Task 011: Done

--- src/main/java/acme/entities/configuration/Configuration.java
+++ src/main/java/acme/entities/configuration/Configuration.java
@@ -1,5 +1,8 @@
 package acme.entities.configuration;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -8,13 +11,14 @@ import org.hibernate.validator.constraints.Range;
 import org.springframework.data.util.Pair;
 
 import acme.framework.entities.AbstractEntity;
+ 
 
 public class Configuration extends AbstractEntity  {
 
 	private static final long serialVersionUID = 1L;
-	
+	//List<Pair<String, Double>>
 	@NotBlank
-	protected Pair spamWords;
+	protected String spamWords;
 	
 	@NotNull
 	@Range(min = 0, max = 1)
@@ -28,4 +32,43 @@ public class Configuration extends AbstractEntity  {
 	@Pattern(regexp = "([A-Z]{3})(,\\s*[A-Z]{3})*")
 	protected String acceptedCurr;
 
+	public boolean isSpam(final String text) {
+		final String[] lowerCaseText = text.toLowerCase().replaceAll("\\s+", " ").split(" ");
+		int spamCount = 0;
+		this.spamWords.replace("(", "");
+		this.spamWords.replace(")", "");
+		List<Pair<String, Double>> ls = new ArrayList<Pair<String, Double>>();
+		
+		final String[] sp = this.spamWords.split(",");
+		
+		for (final String s : sp) {
+			
+			this.spamWords.replace("“", "");
+			this.spamWords.replace("”", "");
+			String[] ss = s.split(";");
+			ls.add(Pair.of(ss[0], Double.parseDouble(ss[1])));
+		
+		}
+		
+		for (final Pair<String, Double> s : ls) {
+			if (text.toLowerCase().trim().replaceAll("\\s+", " ").contains(s.getFirst())) {
+				spamCount++;
+			}
+			for (int i = 0; i < lowerCaseText.length; i++) {
+				if (lowerCaseText[i].contains(s.getFirst())) {
+					spamCount++;
+				}
+
+			}
+		}
+		if (spamCount % 2 == 0) {
+			spamCount = spamCount / 2;
+		} else {
+			spamCount = (spamCount / 2) + 1;
+		}
+		final Double umbral = (double) spamCount / lowerCaseText.length;
+
+		return umbral > this.spamThreshold;
+
+	}
 }
